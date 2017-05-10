7fc3f25ffa33b2f30aa46742c2ca2b6798213bdb
jshields <shields.h.josh@gmail.com>
2017-05-09 22:03:06 -0500

    add support for super cow powers AKA                  (__)                  (oo)            /------\/           / |    ||          *  /\---/\             ~~   ~~ ..."Have you mooed today?"... and the eyes option

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-cowsay for web
+# cowsay for web
+
+## Example usages:
+* Default: [http://cowsay.club/](http://cowsay.club/)
+* With custom text: [http://cowsay.club/?Hello%20World](http://cowsay.club/?Hello%20World)
+* With custom text and eyes: [http://cowsay.club/?Now we're talking&eyes=**](http://cowsay.club/?Now we're talking&eyes=**)
+* With custom eyes and super cow powers: [http://cowsay.club/?eyes=XX&apt-get=moo](http://cowsay.club/?eyes=XX&apt-get=moo)
+
+
+TODO:
+favicon
+expand text box according to content
+allow editing of the text once it has been loaded from query string
+use a template format that is more clear to read
--- js/cowsay.js
+++ js/cowsay.js
@@ -1,23 +1,68 @@
 'use strict';
 
 /*
-TODO:
-expand text box according to content
-allow editing of the text once it has been loaded from query string
-use a template format that is more clear to read
+when browsers support it, these can be used, or bother putting in babel:
+https://developer.mozilla.org/en-US/docs/Web/API/URL/searchParams
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 */
 
-window.addEventListener('load', function (ev) {
-    var cow = document.getElementById('cow');
-    var say = decodeURI(window.location.search.substr(1)) || 'Moo!';
-    cow.innerText = (
-        ' ______\n' +
-        '< ' + say + ' >\n' +
-        ' ------\n' +
-        '        \\   ^__^\n' +
-        '         \\  (oo)\\_______\n' +
-        '            (__)\\       )\\/\\\n' +
-        '                ||----w |\n' +
-        '                ||     ||\n'
-    );
-});
+(function() {
+    window.addEventListener('load', function (ev) {
+        var cow = document.getElementById('cow');
+
+        var pairs = decodeURI(window.location.search).substr(1).split('&');
+        var args = {};
+
+        var i = pairs.length;
+        while (i--) {
+            // arg may be named (e.g. foo=bar)
+            // or unnamed and will become the `defaultArg`
+            var arg = pairs[i].split('=');
+            if (arg.length === 2) {
+                args[arg[0]] = arg[1];
+            } else if (arg.length === 1) {
+                args.defaultArg = arg[0];
+            } else {
+                console.error('Query string invalid.');
+            }
+        }
+
+        var superCowPowers = args['apt-get'] === 'moo';
+
+        var defaultSay = 'Moo!';
+
+        if (superCowPowers) {
+            defaultSay = 'Have you mooed today?';
+        }
+
+        var say = args.defaultArg || args.say || defaultSay;
+        var eyes = args.eyes || 'oo';
+
+
+        var cowsay = (
+            ' ______\n' +
+            '< ' + say + ' >\n' +
+            ' ------\n' +
+            '        \\   ^__^\n' +
+            '         \\  (' + eyes + ')\\_______\n' +
+            '            (__)\\       )\\/\\\n' +
+            '                ||----w |\n' +
+            '                ||     ||\n'
+        );
+
+        var aptGetMoo = (
+            '                 (__) \n' +
+            '                 (' + eyes + ') \n' +
+            '           /------\\/ \n' +
+            '          / |    ||   \n' +
+            '         *  /\\---/\\ \n' +
+            '            ~~   ~~   \n' +
+            '..."' + say + '"...\n'
+        );
+
+        cow.innerText = superCowPowers ? aptGetMoo : cowsay;
+
+    });
+})();
+
+
